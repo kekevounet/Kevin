de672da5e2ac4b9bd4fab21717315f85d69bb137
kekevounet <niavo.kevin9@gmail.com>
2025-02-21 09:44:23 +0300

    Modif du resp phone fa tsy ary mbola vita tsara fa maika fotsiny

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
-// le logo miovaova milay kely no mijery azy
-$(function ()
-{
-    // facebook
-    $('.fa-facebook-square').hide();
-    $('.fa-facebook-f').mouseenter(function () { 
-        $(this).toggle();
-        $('.fa-facebook-square').toggle();
-    });
-    $('.fa-facebook-square').mouseleave(function () {
-        $(this).toggle();
-        $('.fa-facebook-f').toggle();
-    });
+// // le logo miovaova milay kely no mijery azy
+// $(function ()
+// {
+//     // facebook
+//     $('.fa-facebook-square').hide();
+//     $('.fa-facebook-f').mouseenter(function () { 
+//         $(this).toggle();
+//         $('.fa-facebook-square').toggle();
+//     });
+//     $('.fa-facebook-square').mouseleave(function () {
+//         $(this).toggle();
+//         $('.fa-facebook-f').toggle();
+//     });
 
-    // whatsapp
-    $('.fa-whatsapp-square').hide();
-    $('.fa-whatsapp').mouseenter(function () {
-        $(this).toggle();
-        $('.fa-whatsapp-square').toggle();
-    });
-    $('.fa-whatsapp-square').mouseleave(function () {
-        $(this).toggle();
-        $('.fa-whatsapp').toggle();
-    });
-    // linkedin
-    $('.fa-linkedin').hide();
-    $('.fa-linkedin-in').mouseenter(function () {
-        $(this).toggle();
-        $('.fa-linkedin').toggle();
-    });
-    $('.fa-linkedin').mouseleave(function () {
-        $(this).toggle();
-        $('.fa-linkedin-in').toggle();
-    });
-    // mail
-    $('.fa-envelope-open').hide();
-    $('.fa-envelope').mouseenter(function () {
-        $(this).toggle();
-        $('.fa-envelope-open').toggle();
-    });
-    $('.fa-envelope-open').mouseleave(function () {
-        $(this).toggle();
-        $('.fa-envelope').toggle();
-    });
-});
+//     // whatsapp
+//     $('.fa-whatsapp-square').hide();
+//     $('.fa-whatsapp').mouseenter(function () {
+//         $(this).toggle();
+//         $('.fa-whatsapp-square').toggle();
+//     });
+//     $('.fa-whatsapp-square').mouseleave(function () {
+//         $(this).toggle();
+//         $('.fa-whatsapp').toggle();
+//     });
+//     // linkedin
+//     $('.fa-linkedin').hide();
+//     $('.fa-linkedin-in').mouseenter(function () {
+//         $(this).toggle();
+//         $('.fa-linkedin').toggle();
+//     });
+//     $('.fa-linkedin').mouseleave(function () {
+//         $(this).toggle();
+//         $('.fa-linkedin-in').toggle();
+//     });
+//     // mail
+//     $('.fa-envelope-open').hide();
+//     $('.fa-envelope').mouseenter(function () {
+//         $(this).toggle();
+//         $('.fa-envelope-open').toggle();
+//     });
+//     $('.fa-envelope-open').mouseleave(function () {
+//         $(this).toggle();
+//         $('.fa-envelope').toggle();
+//     });
+// });
 
 // CV
 function cv()
@@ -174,4 +174,67 @@ $(function ()
         $('.sidebars').fadeOut();
         $('body').css('overflow', 'visible');
     });
+});
+
+
+// le resp an le page
+$(document).ready(function () {
+    let currentPage = 1;
+    const totalPages = 3;
+
+    // Fonction pour changer de page
+    function changePage() {
+        // Cacher tout le contenu
+        $(".page-content").hide();
+
+        // Afficher le contenu de la page active
+        if (currentPage === 1) {
+            $("#content-accueil").fadeIn();
+        } else if (currentPage === 2) {
+            $("#content-portfolio").fadeIn();
+        } else if (currentPage === 3) {
+            $("#content-experiences").fadeIn();
+        }
+    }
+
+    // Fonction pour passer à la page suivante
+    function nextPage() {
+        if (currentPage < totalPages) {
+            currentPage++;
+            changePage();
+        } else {
+            // Retour à la première page
+            currentPage = 1;
+            changePage();
+        }
+    }
+
+    // Fonction pour revenir à la page précédente
+    function prevPage() {
+        if (currentPage > 1) {
+            currentPage--;
+            changePage();
+        } else {
+            // Aller à la dernière page si on est sur la première
+            currentPage = totalPages;
+            changePage();
+        }
+    }
+
+    // Clic gauche ou droite pour changer de page
+    $('#phone_fond').click(function (e) {
+        const screenWidth = $(this).width();
+        const clickPosition = e.pageX;
+
+        if (clickPosition < screenWidth / 2) {
+            // Clic à gauche : page précédente
+            prevPage();
+        } else {
+            // Clic à droite : page suivante
+            nextPage();
+        }
+    });
+
+    // Initialiser la première page
+    changePage();
 });
