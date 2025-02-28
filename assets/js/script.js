d7258bfe27279fdea67fd5c635d3d02e4301e7e9
kekevounet <niavo.kevin9@gmail.com>
2025-02-28 09:42:28 +0300

    vaovao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,240 +1,88 @@
-// // le logo miovaova milay kely no mijery azy
-// $(function ()
-// {
-//     // facebook
-//     $('.fa-facebook-square').hide();
-//     $('.fa-facebook-f').mouseenter(function () { 
-//         $(this).toggle();
-//         $('.fa-facebook-square').toggle();
-//     });
-//     $('.fa-facebook-square').mouseleave(function () {
-//         $(this).toggle();
-//         $('.fa-facebook-f').toggle();
-//     });
+// le acceuil miafina ito
+$(function () {
+   
+    $('.navbar_menu').css('top', '-100px');
 
-//     // whatsapp
-//     $('.fa-whatsapp-square').hide();
-//     $('.fa-whatsapp').mouseenter(function () {
-//         $(this).toggle();
-//         $('.fa-whatsapp-square').toggle();
-//     });
-//     $('.fa-whatsapp-square').mouseleave(function () {
-//         $(this).toggle();
-//         $('.fa-whatsapp').toggle();
-//     });
-//     // linkedin
-//     $('.fa-linkedin').hide();
-//     $('.fa-linkedin-in').mouseenter(function () {
-//         $(this).toggle();
-//         $('.fa-linkedin').toggle();
-//     });
-//     $('.fa-linkedin').mouseleave(function () {
-//         $(this).toggle();
-//         $('.fa-linkedin-in').toggle();
-//     });
-//     // mail
-//     $('.fa-envelope-open').hide();
-//     $('.fa-envelope').mouseenter(function () {
-//         $(this).toggle();
-//         $('.fa-envelope-open').toggle();
-//     });
-//     $('.fa-envelope-open').mouseleave(function () {
-//         $(this).toggle();
-//         $('.fa-envelope').toggle();
-//     });
-// });
+    $(window).scroll(function () {
+        var currentScrollTop = $(this).scrollTop();
 
-// CV
-function cv()
-{
-    alert('Oups!! Une erreur est survenue, réessayer plus tard.')
-}
 
-// Smile
+        if (currentScrollTop === 0) {
+            $('.navbar_menu').css('top', '-100px');
+        } else {
+
+            $('.navbar_menu').css('top', '0');
+        }
+
+        lastScrollTop = currentScrollTop;
+    });
+});
+// le thème no eto
 $(function ()
 {
-    $('.smile2').hide();
-    $('.smile3').hide();
-    $('.smile1').mouseenter(function () { 
+    // mode mazava 
+    $('.fa-moon').hide();
+    $('.fa-sun').click(function ()
+    {
         $(this).toggle();
-        $('.smile2').toggle();
+        $('.fa-moon').toggle();
+        $('body').addClass('body_theme');
+        $('.navbar_menu').addClass('navbar_menu_theme');
+        $('.menu_liste_a').addClass('menu_liste_a_theme');
+        $('.lienlogo').addClass('menu_liste_a_theme');
+        $('.img_conteneur').addClass('img_conteneur_theme');
+        $('.whatsapp').addClass('whatsapp_theme');
+        $('.facebook').addClass('facebook_theme');
+        $('.linkedin').addClass('linkedin_theme');
+        $('.mail').addClass('mail_theme');
+        $('.mon_competence').addClass('mon_competence_theme');
+        $('.titre_competence').addClass('titre_competence_theme');
+        $('.menu_liste_a.active').addClass('menu_liste_a_theme.active');
+        $('.timeline').addClass('timeline_theme');
+        $('.exp_1').addClass('exp_1_theme');
+        $('.exp_2').addClass('exp_2_theme');
+        $('.exp_3').addClass('exp_3_theme');
+        $('.fa-briefcase').addClass('fa-briefcase_theme');
+        $('.timeline_icon_3').addClass('timeline_icon_3_theme');
+        $('.timeline_icon_2').addClass('timeline_icon_2_theme');
+        $('.timeline_icon_1').addClass('timeline_icon_1_theme');
     });
-    $('.smile2').mouseleave(function () {
+
+
+
+
+    // mode sombre
+    $('.fa-moon').click(function ()
+    {
         $(this).toggle();
-        $('.smile1').toggle();
-    });
-    $('.smile2').mousedown(function ()
-    {  
-        $(this).hide();
-        $('.smile1').hide();
-        $('.smile3').fadeIn();
-    });
-    $('.smile3').mousedown(function () {
-        $(this).hide();
-        $('.smile1').hide();
-        $('.smile2').fadeIn();
-    });
-});
-// $(function ()
-// {
-//     $('.marque').addClass('test');
-// });
-// le style an le lien vo mcilque zany le ahafantarana an le lien
-$(function ()
-{
-    $('#marque1').addClass('menu_liste_lien1_focus');
-    $('#marque1').click(function ()
-{
-        $('#marque2').removeClass('menu_liste_lien1_focus');
-        $('#marque3').removeClass('menu_liste_lien1_focus');
-        $('#marque4').removeClass('menu_liste_lien1_focus');
-        $('#marque1').addClass('menu_liste_lien1_focus');
-    });
-    $('#marque2').click(function ()
-    {
-        $('#marque1').removeClass('menu_liste_lien1_focus');
-        $('#marque3').removeClass('menu_liste_lien1_focus');
-        $('#marque4').removeClass('menu_liste_lien1_focus');
-        $('#marque2').addClass('menu_liste_lien1_focus');
-    });
-    $('#marque3').click(function ()
-    {
-        $('#marque1').removeClass('menu_liste_lien1_focus');
-        $('#marque2').removeClass('menu_liste_lien1_focus');
-        $('#marque4').removeClass('menu_liste_lien1_focus');
-        $('#marque3').addClass('menu_liste_lien1_focus');
-    });
-    $('#marque4').click(function ()
-    {
-        $('#marque1').removeClass('menu_liste_lien1_focus');
-        $('#marque2').removeClass('menu_liste_lien1_focus');
-        $('#marque3').removeClass('menu_liste_lien1_focus');
-        $('#marque4').addClass('menu_liste_lien1_focus');
-    });
-});
-// Animation mihetsiketsika une fois mclique
-$(function ()
-{
-    $('.mon_portfolio').click(function ()
-    {
-        $(this).toggleClass('click_animation');
-    });
-    $('.mon_nom').click(function ()
-    {
-        $(this).toggleClass('click_animation');
-    });
-    $('.still_text').click(function ()
-    {
-        $(this).toggleClass('click_animation');
-    });
-    $('.bienvenue_texte').click(function ()
-    {
-        $(this).toggleClass('click_animation');
-    });
-    $('.mon_job').click(function ()
-    {
-        $(this).toggleClass('mon_job_anim');
-    });
-    $('.bienvenue_texte_1').click(function ()
-    {
-        $(this).toggleClass('mon_job_anim');
-    });
-});
-// le menu mikisaka an le tabletty sy phone 
-$(function ()
-{
-    $('.menu_resp_me').click(function ()
-    {
-        $('.sidebars').fadeIn();
-        $('body').css('overflow', 'hidden');
-        $('.sidebars').addClass('sidebars_btn');
-        $('.menu_liste').hide();
-        $('.momba_ahy').show();
-        $('.menu_resp_me_hide').css('display', 'block');
-        $('.img_me_conteneur').css
-            ({
-                'width': '230px',
-                'height':'230px'
-            });
-        $('.reseau_sociaux').css
-            ({
-                'margin': '0px'
-            });
+        $('.fa-sun').toggle();
+        $('body').removeClass('body_theme');
+        $('.navbar_menu').removeClass('navbar_menu_theme');
+        $('.menu_liste_a').removeClass('menu_liste_a_theme');
+        $('.lienlogo').removeClass('menu_liste_a_theme');
+        $('.img_conteneur').removeClass('img_conteneur_theme');
+        $('.whatsapp').removeClass('whatsapp_theme');
+        $('.facebook').removeClass('facebook_theme');
+        $('.linkedin').removeClass('linkedin_theme');
+        $('.mail').removeClass('mail_theme');
+        $('.mon_competence').removeClass('mon_competence_theme');
+        $('.titre_competence').removeClass('titre_competence_theme');
+        $('.menu_liste_a.active').removeClass('menu_liste_a_theme.active');
+        $('.timeline').removeClass('timeline_theme');
+        $('.exp_1').removeClass('exp_1_theme');
+        $('.exp_2').removeClass('exp_2_theme');
+        $('.exp_3').removeClass('exp_3_theme');
+        $('.fa-briefcase').removeClass('fa-briefcase_theme');
+        $('.timeline_icon_3').removeClass('timeline_icon_3_theme');
+        $('.timeline_icon_2').removeClass('timeline_icon_2_theme');
+        $('.timeline_icon_1').removeClass('timeline_icon_1_theme');
+
+
+
         
-        $('.still').css
-            ({
-                'display': 'flex',
-                'position':'static',
-                'justify-content': 'center',
 
-            });  
-    });
-    $('.menu_resp_me_hide').click(function ()
-    {
-        $('.sidebars').fadeOut();
-        $('body').css('overflow', 'visible');
     });
 });
-
-
-// le resp an le page
-$(document).ready(function () {
-    let currentPage = 1;
-    const totalPages = 2;
-
-    // Fonction pour changer de page
-    function changePage() {
-        // Cacher tout le contenu
-        $(".page-content").hide();
-
-        // Afficher le contenu de la page active
-        if (currentPage === 1) {
-            $("#content-accueil").fadeIn();
-        } else if (currentPage === 2) {
-            $("#content-portfolio").fadeIn();
-         } //else if (currentPage === 3) {
-        //     $("#content-experiences").fadeIn();
-        // }
-    }
-
-    // Fonction pour passer à la page suivante
-    function nextPage() {
-        if (currentPage < totalPages) {
-            currentPage++;
-            changePage();
-        } else {
-            // Retour à la première page
-            currentPage = 1;
-            changePage();
-        }
-    }
-
-    // Fonction pour revenir à la page précédente
-    function prevPage() {
-        if (currentPage > 1) {
-            currentPage--;
-            changePage();
-        } else {
-            // Aller à la dernière page si on est sur la première
-            currentPage = totalPages;
-            changePage();
-        }
-    }
-
-    // Clic gauche ou droite pour changer de page
-    $('#phone_fond').click(function (e) {
-        const screenWidth = $(this).width();
-        const clickPosition = e.pageX;
-
-        if (clickPosition < screenWidth / 2) {
-            // Clic à gauche : page précédente
-            prevPage();
-        } else {
-            // Clic à droite : page suivante
-            nextPage();
-        }
-    });
-
-    // Initialiser la première page
-    changePage();
+$(window).on('load', function () {
+    $('#preloader').fadeOut('slow');
 });
